b00d0e8b5319ce6eaab43e1d4699a8cdeb17b40d
hectormdp <hchaves@gmail.com>
2016-10-05 10:21:12 -0300

    aca voy

--- src/application/controladores/AppControlador.java
+++ src/application/controladores/AppControlador.java
@@ -22,7 +22,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
-
+///comentario
 public class AppControlador implements Initializable {
 
 	App app;
