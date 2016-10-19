21a4234429e012c02980f7581396776aa1184107
matias <mss1982_23@hotmail.com>
2016-10-19 10:29:34 -0300

    Archivo script-popular.sql.

---
+++ src/application/basededatos/script-popular.sql
@@ -0,0 +1,22 @@
+INSERT INTO Sexo VALUES ('Masculino')
+INSERT INTO Sexo VALUES ('Femenino')
+
+INSERT INTO EstadoCivil VALUES ('Casado')
+INSERT INTO EstadoCivil VALUES ('Soltero')
+INSERT INTO EstadoCivil VALUES ('Viudo')
+INSERT INTO EstadoCivil VALUES ('Divorciado')
+INSERT INTO EstadoCivil VALUES ('Concubino')
+
+INSERT INTO Nivel VALUES ('Administrador')
+INSERT INTO Nivel VALUES ('Invitado')
+
+INSERT INTO TipoEmpleado VALUES ('Gerente')
+INSERT INTO TipoEmpleado VALUES ('Junior')
+
+
+
+
+
+
+
+GO
